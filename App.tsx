14030575c0e949fc9a7874692fb9413449390a75
anshuman1490 <anshuman.s1490@gmail.com>
2022-12-29 14:55:05 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,330 @@
+import * as React from 'react';
+import './style.css';
+import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material/styles';
+import BasicCard from './BasicCard';
+const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#0E6BF1',
+      dark: '#0B55C0',
+      light: '#E2EDFD',
+    },
+    secondary: {
+      main: '#232830',
+      dark: '#0B111A',
+      light: '#F2F2F2',
+    },
+    error: {
+      main: '#E5415E',
+      dark: '#C91B3A',
+      light: '#EF8D9E',
+    },
+    warning: {
+      main: '#F2AE24',
+      dark: '#BF8000',
+      light: '#F7CE7B',
+    },
+    info: {
+      main: '#0E6BF1',
+      dark: '#0B55C0',
+      light: '#B2D0FB',
+    },
+    success: {
+      main: '#51C071',
+      dark: '#38854D',
+      light: '#85D29B',
+    },
+    text: {
+      primary: '#232830',
+      secondary: 'rgba(35,40,48,0.6)',
+      disabled: 'rgba(35,40,48,0.4)',
+    },
+    action: {
+      active: 'rgba(0, 0, 0, 0.54)',
+      hover: 'rgba(0, 0, 0, 0.04)',
+      selected: 'rgba(0, 0, 0, 0.08)',
+      disabled: 'rgba(0, 0, 0, 0.26)',
+      disabledBackground: 'rgba(0, 0, 0, 0.12)',
+      focus: 'rgba(0, 0, 0, 0.12)',
+    },
+    background: {
+      paper: '#FFFFFF',
+      default: '#FFFFFF',
+    },
+    divider: 'rgba(0, 0, 0, 0.12)',
+  },
+  typography: {
+    h1: {
+      fontWeight: 500,
+      fontSize: 96,
+      lineHeight: '116.7%',
+      fontFamily: 'Hellix',
+      letterSpacing: -1.5,
+    },
+    h2: {
+      fontWeight: 500,
+      fontSize: 60,
+      fontFamily: 'Hellix',
+      lineHeight: '120%',
+      letterSpacing: -0.5,
+    },
+    h3: {
+      fontWeight: 600,
+      fontSize: 32,
+      lineHeight: '116.7%',
+      fontFamily: 'Hellix',
+    },
+    h4: {
+      fontWeight: 600,
+      fontSize: 24,
+      fontFamily: 'Hellix',
+      lineHeight: '123.5%',
+      letterSpacing: 0.25,
+    },
+    h5: {
+      fontWeight: 600,
+      fontSize: 20,
+      lineHeight: '133.4%',
+      fontFamily: 'Hellix',
+    },
+    h6: {
+      fontWeight: 500,
+      fontSize: 16,
+      lineHeight: '160%',
+      letterSpacing: 0.15,
+      fontFamily: 'Hellix',
+    },
+    subtitle1: {
+      fontWeight: 700,
+      fontSize: 16,
+      lineHeight: '175%',
+      letterSpacing: 0.15,
+      fontFamily: 'Arial',
+    },
+    subtitle2: {
+      fontWeight: 400,
+      fontSize: 14,
+      lineHeight: '157%',
+      letterSpacing: 0.1,
+      fontFamily: 'Arial',
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: 16,
+      lineHeight: '150%',
+      letterSpacing: 0.15,
+      fontFamily: 'Arial',
+    },
+    body2: {
+      fontWeight: 400,
+      fontSize: 14,
+      lineHeight: '143%',
+      letterSpacing: 0.17,
+      fontFamily: 'Arial',
+    },
+    caption: {
+      fontWeight: 400,
+      fontSize: 12,
+      lineHeight: '166%',
+      letterSpacing: 0.4,
+      fontFamily: 'Arial',
+    },
+    overline: {
+      fontWeight: 400,
+      fontSize: 12,
+      lineHeight: '266%',
+      letterSpacing: 1,
+      fontFamily: 'Arial',
+    },
+    button: {
+      fontWeight: 700,
+      fontFamily: 'Arial',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        containedSizeMedium: {
+          borderRadius: '16px',
+          fontSize: '14px',
+          letterSpacing: '0.4px',
+          lineHeight: '171%',
+        },
+        containedSizeLarge: {
+          borderRadius: '20px',
+          fontSize: '15px',
+          letterSpacing: '0.46px',
+          lineHeight: '186%',
+        },
+        containedSizeSmall: {
+          borderRadius: '12px',
+          fontSize: '13px',
+          letterSpacing: '0.46px',
+          lineHeight: '183%',
+        },
+        outlinedSizeMedium: {
+          borderRadius: '16px',
+          fontSize: '14px',
+          letterSpacing: '0.4px',
+          lineHeight: '171%',
+        },
+        outlinedSizeLarge: {
+          borderRadius: '20px',
+          fontSize: '15px',
+          letterSpacing: '0.46px',
+          lineHeight: '186%',
+        },
+        outlinedSizeSmall: {
+          borderRadius: '12px',
+          fontSize: '13px',
+          letterSpacing: '0.46px',
+          lineHeight: '183%',
+        },
+        textSizeMedium: {
+          borderRadius: '16px',
+          fontSize: '14px',
+          letterSpacing: '0.4px',
+          lineHeight: '171%',
+        },
+        textSizeLarge: {
+          borderRadius: '20px',
+          fontSize: '15px',
+          letterSpacing: '0.46px',
+          lineHeight: '186%',
+        },
+        textSizeSmall: {
+          borderRadius: '12px',
+          fontSize: '13px',
+          letterSpacing: '0.46px',
+          lineHeight: '183%',
+        },
+        outlined: {
+          border: '1px solid rgba(0, 0, 0, 0.23)',
+        },
+        root: {
+          textTransform: "none"
+        }
+      },
+    },
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+          letterSpacing: '0.14px',
+          fontFamily: 'Hellix',
+          fontSize: '20px',
+          lineHeight: '100%',
+          fontWeight: 400,
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          letterSpacing: '0.15px',
+          fontFamily: 'Arial',
+          fontSize: '12px',
+          lineHeight: '12px',
+          fontWeight: 400,
+        },
+      },
+    },
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          letterSpacing: '0.15px',
+          fontFamily: 'Arial',
+          fontSize: '14px',
+          lineHeight: '24px',
+          fontWeight: 400,
+        },
+      },
+    },
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          letterSpacing: '0.15px',
+          fontFamily: 'Arial',
+          fontSize: '14px',
+          lineHeight: '24px',
+          fontWeight: 400,
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          letterSpacing: '0.15px',
+          fontFamily: 'Arial',
+          fontSize: '14px',
+          lineHeight: '24px',
+          fontWeight: 400,
+          border: '1px solid rgba(0, 0, 0, 0.23);',
+        },
+      },
+    },
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          fontFamily: 'Arial',
+          fontSize: '10px',
+          lineHeight: '14px',
+          fontWeight: 400,
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'Arial',
+          fontSize: '12px',
+          lineHeight: '20px',
+          fontWeight: 400,
+          letterSpacing: '0.4px',
+        },
+      },
+    },
+    MuiChip: {
+      styleOverrides: {
+        outlined: {
+          border: '1px solid rgba(0, 0, 0, 0.23)',
+        },
+      },
+    },
+    MuiBackdrop: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+    MuiRating: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#FFB400',
+        },
+      },
+    },
+    MuiSnackbar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#323232',
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        outlined: {
+          border: '1px solid rgba(35, 40, 48, 0.12)',
+        },
+      },
+    },
+  },
+};
+const wfTheme = createTheme(themeOptions);
+export default function App() {
+  return (
+    <ThemeProvider theme={wfTheme}>
+      <BasicCard />
+    </ThemeProvider>
+  );
+}
---
+++ BasicCard.tsx
@@ -0,0 +1,147 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Switch from '@mui/material/Switch';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+const card = ({ id, workerName, version, desc, isSwitchEnable }) => (
+  <React.Fragment>
+    <CardContent>
+      {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography> */}
+      <Box sx={{ display: 'flex' }}>
+        <Box sx={{ flexGrow: 0 }}>
+          <svg
+            width="20"
+            height="22"
+            viewBox="0 0 20 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.5 4.13878L16.9667 2.97767L15.8056 0.444336L14.6445 2.97767L12.1112 4.13878L14.6445 5.29989L15.8056 7.83323L16.9667 5.29989L19.5 4.13878Z"
+              fill="#CECFD1"
+            />
+            <path
+              d="M8.94444 6.67213C10.1689 6.67213 11.1611 7.66435 11.1611 8.88879C11.1611 10.1132 10.1689 11.1055 8.94444 11.1055C7.72 11.1055 6.72778 10.1132 6.72778 8.88879C6.72778 7.66435 7.72 6.67213 8.94444 6.67213ZM8.94444 16.1721C12.0794 16.1721 15.3833 17.7132 15.3833 18.3888V19.5499H2.50556V18.3888C2.50556 17.7132 5.80944 16.1721 8.94444 16.1721ZM8.94444 4.66657C6.61167 4.66657 4.72222 6.55602 4.72222 8.88879C4.72222 11.2216 6.61167 13.111 8.94444 13.111C11.2772 13.111 13.1667 11.2216 13.1667 8.88879C13.1667 6.55602 11.2772 4.66657 8.94444 4.66657ZM8.94444 14.1666C6.12611 14.1666 0.5 15.581 0.5 18.3888V20.4999C0.5 21.0805 0.975 21.5555 1.55556 21.5555H16.3333C16.9139 21.5555 17.3889 21.0805 17.3889 20.4999V18.3888C17.3889 15.581 11.7628 14.1666 8.94444 14.1666Z"
+              fill="#CECFD1"
+            />
+          </svg>
+        </Box>
+        <Typography
+          sx={{ flexGrow: '1', mx: '10px' }}
+          variant="h5"
+          component="div"
+        >
+          {workerName}
+        </Typography>
+        {isSwitchEnable ? (
+          <Switch {...label} defaultChecked />
+        ) : (
+          <Switch {...label} />
+        )}
+      </Box>
+
+      <Typography variant="body1" color="text.secondary">
+        {version} {bull} 4 Feb 2022
+      </Typography>
+      <Typography variant="body1">{desc}</Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">
+        Details
+        <Box sx={{ ml: '10px', mt: '5px' }}>
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.00016 0.333374L5.82516 1.50837L10.4752 6.16671H0.333496V7.83337H10.4752L5.82516 12.4917L7.00016 13.6667L13.6668 7.00004L7.00016 0.333374Z"
+              fill="#0E6BF1"
+            />
+          </svg>
+        </Box>
+      </Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+function CollapsedBreadcrumbs() {
+  return (
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="#">
+          Solution Catalog
+        </Link>
+        <Typography color="text.primary">Workers</Typography>
+      </Breadcrumbs>
+    </div>
+  );
+}
+export default function BasicCard() {
+  const cardDetails = [
+    {
+      workerName: 'Evelyn',
+      version: '1.2',
+      desc: 'Evelyn is a top expert in BSA/OFAC requirements, with a passion for adverse media monitoring, name screening and sanction screening.',
+      isSwitchEnable: true,
+      id: '123qwe',
+    },
+    {
+      workerName: 'Darryl',
+      version: '1.2',
+      desc: 'Verify an account applicant’s identity through submitted documentation and data faster with WorkFusion® Intelligent Automation.',
+      isSwitchEnable: true,
+      id: '123abc',
+    },
+    {
+      workerName: 'Tara',
+      version: '1.0',
+      desc: 'Tara is a top OFAC / AML expert who is laser-focused on keeping your transactions risk-free.',
+      isSwitchEnable: false,
+      id: '123dfg',
+    },
+  ];
+  const CardMap = cardDetails.map((item) => {
+    return <CustomCard key={item.id} {...item} />;
+  });
+  return (
+    <Box>
+      <CollapsedBreadcrumbs />
+      <Typography sx={{ mx: '20px' }} variant="h5" component="div">
+        Worker
+      </Typography>
+      {CardMap}
+    </Box>
+  );
+}
+function CustomCard(props) {
+  return (
+    <Box sx={{ minWidth: 275, m: '24px', display: 'flex', flexWrap: 'wrap' }}>
+      <Card variant="outlined">{card(props)}</Card>
+    </Box>
+  );
+}
